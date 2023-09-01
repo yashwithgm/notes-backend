e18492c1719d91bacc9410a8429dc2be0c5f05f3
Yash Verma <yashchrono@gmail.com>
2023-09-01 10:49:41 +0530

    Added POST handler for notes

--- index.js
+++ index.js
@@ -13,13 +13,8 @@ app.use(express.json());
 const Note = require('./models/note');
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find((note) => note.id === id);
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    Note.findById(request.params.id)
+        .then(note => response.json(note));
 });
 
 app.get('/api/notes', (request, response) => {
@@ -45,18 +40,18 @@ app.post('/api/notes', (request, response) => {
         });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    };
+    });
 
-    notes = notes.concat(note);
-    response.json(note);
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    });
+
 });
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
     notes = notes.filter(note => note.id !== id);
     response.status(204).end();
 });
